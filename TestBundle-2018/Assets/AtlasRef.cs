d03fa7800a1426fd28c13d2c33ca9c337238145e
Fallever <piscovip@163.com>
2023-12-04 14:47:57 +0800

    M 测试结果
    Signed-off-by: Fallever <piscovip@163.com>

---
+++ TestBundle-2018/Assets/AtlasRef.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class AtlasRef : MonoBehaviour
+{
+    public SpriteAtlas atlas;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+#if UNITY_EDITOR
+
+    [UnityEditor.MenuItem("Tools/ClearCache")]
+    static void ClearCache()
+    {
+        Caching.ClearCache();
+    }
+#  endif
+}
